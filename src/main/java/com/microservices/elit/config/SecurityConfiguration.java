8eb18916722eedd1bb90d766aa888d9c35e08c6d
360gunner <hkentache@gmail.com>
2020-03-15 14:00:42 +0100

    roles updated

--- src/main/java/com/microservices/elit/config/SecurityConfiguration.java
+++ src/main/java/com/microservices/elit/config/SecurityConfiguration.java
@@ -30,16 +30,37 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .passwordEncoder(getPasswordEncoder());
     }
 
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests()
+   http
+        .authorizeRequests()
+        .antMatchers("/mira/tt2").hasRole("ADMIN")
+        .and()
+        .authorizeRequests()
                 .antMatchers("login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll();
+       /* .and().antMatcher("/mira/tt2")                               
+        .authorizeRequests()
+        .anyRequest().hasRole("ADMIN");*/
+       // http.authorizeRequests().antMatchers("/mira/tt2").hasAnyRole("ADMIN");
+        
+        /*
+        http
+        .authorizeRequests()
+            .antMatchers("/high_level_url_A/sub_level_1").hasRole('USER')
+            .antMatchers("/high_level_url_A/sub_level_2").hasRole('USER2')
+            .somethingElse() // for /high_level_url_A/**
+            .antMatchers("/high_level_url_A/**").authenticated()
+            .antMatchers("/high_level_url_B/sub_level_1").permitAll()
+            .antMatchers("/high_level_url_B/sub_level_2").hasRole('USER3')
+            .somethingElse() // for /high_level_url_B/**
+            .antMatchers("/high_level_url_B/**").authenticated()
+            .anyRequest().permitAll()  */
     }
 
     private PasswordEncoder getPasswordEncoder() {
