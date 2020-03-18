9ea17c4df3aea4ae6cc902fedce1a35fc570d505
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-03-18 23:38:42 +0100

    Update DB_Dump.sql

--- DB_Dump.sql
+++ DB_Dump.sql
@@ -7,34 +7,31 @@
 #one to many relation.. 
 ####################################
 ####################################
-
 DROP TABLE IF EXISTS `role`;
-
 CREATE TABLE `role` (
   `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-
 INSERT INTO `role` (`role_id`, `role`)
 VALUES
 	(1,'ADMIN');
 INSERT INTO `role` (`role_id`, `role`)
 VALUES
 	(2,'USER');
-
+INSERT INTO `role` (`role_id`, `role`)
+VALUES
+	(3,'WRITER');
+INSERT INTO `role` (`role_id`, `role`)
+VALUES
+	(4,'READER');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
 # Dump of table user
 # ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `user`;
-
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `active` int(11) DEFAULT NULL,
@@ -44,48 +41,42 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-
 INSERT INTO `user` (`user_id`, `active`, `email`, `last_name`, `name`, `password`)
 VALUES
 	(1,1,'houssem@gmail.com','houssem','360gunner','1998'),
 	(2,1,'amira@gmail.com','amira','amira','bellili');
-
-
-
-
+INSERT INTO `user` (`user_id`, `active`, `email`, `last_name`, `name`, `password`)
+VALUES
+	(3,1,'moh@gmail.com','mohammed','moh','elit');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
 # Dump of table user_role
 # ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `user_role`;
-
 CREATE TABLE `user_role` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
   CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-
 INSERT INTO `user_role` (`user_id`, `role_id`)
 VALUES
 	(1,1),
+    (1,2),
+    (1,3),
+    (1,4),
     (2,2);
-
+INSERT INTO `user_role` (`user_id`, `role_id`)
+VALUES
+	(3,1),
+    (3,2),
+    (3,4);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
